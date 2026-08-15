c7f45de09bcfbef6877bb02bd94913c98162f081
JEBAS7 <jebas7@outlook.com>
2026-08-15 07:49:59 -0300

    Python POO aula 09 Herança e Abstração Exercícios

--- desafios/desafio027/personagem_rpg.py
+++ desafios/desafio027/personagem_rpg.py
@@ -1,33 +1,31 @@
 import random
+from rich import print
 from abc import ABC, abstractmethod
 
 
 # --- CLASSE MÃE (ABSTRATA) ---
 class Personagem(ABC):
-    def __init__(self, nome: str, vida: int, defesa: int):
+    def __init__(self, nome, vida):
         self.nome = nome
         self.vida = vida
-        self.defesa = defesa
-        self.lista_golpes = []
+        self.golpes = []
 
-    def atacar(self, alvo: 'Personagem', forca: int):
+    def atacar(self, alvo, forca = 100):
+        if self.vida > 0 and alvo.vida > 0:
+            golpe = self.golpes[random.randrange(0, len(self.golpes))]
+            print(f"[green]{self.nome}({self.vida})[/green] atacou [red]{alvo.nome}({alvo.vida})[/red] com um [blue]{golpe}[/blue] de força [yellow]{forca}[/yellow]")
+            alvo.receber_dano(forca)
+        else:
+            print(f'O ataque {self.nome} -> {alvo.nome} não pode acontecer')
+
+    def receber_dano(self, dano):
         from rich import print as rprint
-        golpe_sorteado = random.choice(self.lista_golpes)
-        rprint(
-            f"[green]{self.nome}({self.vida})[/green] atacou [red]{alvo.nome}({alvo.vida})[/red] com um [blue]{golpe_sorteado}[/blue] de força [yellow]{forca}[/yellow]")
-        alvo.receber_dano(forca)
-
-    def receber_dano(self, dano_bruto: int):
-        from rich import print as rprint
-        dano_final = dano_bruto - self.defesa
-        if dano_final < 0:
-            dano_final = 0
-
-        self.vida -= dano_final
+        fator = random.randint(0, dano)
+        self.vida -= fator
         if self.vida < 0:
             self.vida = 0
 
-        rprint(f"[cyan]{self.nome} recebeu dano de {dano_final}![/cyan]")
+        rprint(f"[cyan]{self.nome} recebeu dano de {fator}![/cyan]")
 
     # === NOVO MÉTODO COMPLEMENTAR (DESAFIO 027) ===
     def morreu(self, personagem_alvo: 'Personagem', reviveu: int = 0):
@@ -52,9 +50,9 @@ class Personagem(ABC):
 
 # --- CLASSE FILHA: GUERREIRO ---
 class Guerreiro(Personagem):
-    def __init__(self, nome: str, vida: int = 2000, defesa: int = 12166):
-        super().__init__(nome, vida, defesa)
-        self.lista_golpes = ["Golpe de Machado", "Espadada", "Soco"]
+    def __init__(self, nome, vida):
+        super().__init__(nome, vida)
+        self.golpes = ["Golpe de Machado", "Espadada", "Soco"]
 
     def curar(self):
         from rich import print as rprint
@@ -65,12 +63,12 @@ class Guerreiro(Personagem):
 
 # --- CLASSE FILHA: MAGO ---
 class Mago(Personagem):
-    def __init__(self, nome: str, vida: int = 3000, defesa: int = 724):
-        super().__init__(nome, vida, defesa)
-        self.lista_golpes = ["Bola de Fogo", "Raio Arcano", "Explosão de Gelo"]
+    def __init__(self, nome, vida):
+        super().__init__(nome, vida)
+        self.golpes = ["Bola de Fogo", "Raio Arcano", "Explosão de Gelo"]
 
     def curar(self):
         from rich import print as rprint
-        cura = random.randint(200, 600)
+        cura = random.randint(200, 800)
         self.vida += cura
         rprint(f"[blue]{self.nome} fez uma magia de cura e recuperou {cura} pontos de vida.[/blue]")
